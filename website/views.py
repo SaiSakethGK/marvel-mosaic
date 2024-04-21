adb8e27dff002ba449c54ec2f16cd0cdbecea14f
SaiSakethGK <saisaketh.gootykase@unh.edu>
2024-04-21 18:08:16 -0400

    Add user registration feature

--- website/views.py
+++ website/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from marvel_api import get_marvel_characters, get_character_by_id
 from django.shortcuts import render, get_object_or_404
 from marvel_api import get_marvel_characters, get_character_by_id
-
+from .forms import SignUpForm
 
 
 
@@ -44,3 +44,21 @@ def characters_list(request):
 def character_detail(request, character_id):
     character = get_character_by_id(character_id)
     return render(request, 'character_detail.html', {'character': character})
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})

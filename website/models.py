22defc978389a28ce9e4e5d8084995d7501ad2fa
21jayadeep <jm1916@usnh.edu>
2024-04-22 16:26:14 -0400

    Add model for favorite character

--- website/models.py
+++ website/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class FavoriteCharacter(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    character_id = models.IntegerField()
+
+    
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     character_id = models.IntegerField()
